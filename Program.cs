0aeb7d054b75f4c6d1270e9beceb2f001b2b073c
Ivan M <slade91@mail.ru>
2022-10-12 20:46:00 +0300

    second

--- Program.cs
+++ Program.cs
@@ -4,4 +4,26 @@
 44 5 78 -> 78
 22 3 9 -> 22*/
 
+Console.WriteLine ("Enter the first number");
+string firstNumber = Console.ReadLine ();
 
+Console.WriteLine ("Enter the second number");
+string secondNumber = Console.ReadLine ();
+
+Console.WriteLine ("Enter the third number");
+string thirdNumber = Console.ReadLine ();
+
+int firstNum = int.Parse(firstNumber);
+int secondNum = int.Parse(secondNumber);
+int thirdNum = int.Parse(thirdNumber);
+
+int max = firstNum;
+
+if (firstNum > max) max = firstNum;
+
+if (secondNum > max) max = secondNum;
+
+if (thirdNum > max) max = thirdNum;
+
+Console.Write("max -> ");
+Console.WriteLine(max);
